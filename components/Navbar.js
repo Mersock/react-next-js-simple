05ecaa6a6abb736db266000354969a50228d7b97
Phumthawan <knzphumthawan@gmail.com>
2019-05-24 10:58:08 +0700

    add nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,34 +1,28 @@
 import Link from 'next/link';
 
-const navbar = () => {
-    return (
-        <div>
-            <ul>
-                <li><Link href="/"><a>Home</a></Link></li>
-                <li><Link href="/about"><a>About</a></Link></li>
+const navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link href="/"><a className="navbar-brand" >BitzPrice</a></Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
+                aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <Link href="/"><a className="nav-link" >Home <span className="sr-only">(current)</span></a></Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/about"><a className="nav-link" href="#">Features</a></Link>
+                </li>
             </ul>
-
-            <style jsx>{`
-                ul{
-                    background: #333;
-                    color:#fff;
-                    list-style:none;
-                    display:flex;
-                }
-                ul li {
-                    font-size: 18px;
-                    margin-right: 20px;
-
-                }
-
-                ul li a {
-                   color: #fff;
-                   text-decoration: none;
-                }
-            `}
-            </style>
+            <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="text" placeholder="Search" />
+                    <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
-    )
-};
+    </nav>
+);
 
 export default navbar;
