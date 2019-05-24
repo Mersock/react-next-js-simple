e19de24b1b607f2893dc36689555759dbeb663ff
Phumthawan <knzphumthawan@gmail.com>
2019-05-24 10:30:43 +0700

    initial simple

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const index = () => {
+    return (
+        <Layout>
+            <div>
+                Hello NEXT JS.
+            </div>
+        </Layout>
+    );
+};
+
+export default index;
