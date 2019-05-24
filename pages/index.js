1f592ef761845a95031cc2970ac2d88801ee2384
Phumthawan <knzphumthawan@gmail.com>
2019-05-24 12:29:07 +0700

    add api bitcoin

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
+import Fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
+import Prices from '../components/Prices';
 
-const index = () => {
+const index = (props) => {
     return (
         <Layout>
             <div>
-                Hello NEXT JS.
+            <h1>Hello NEXT JS.</h1>
+            <p>Check current Bitcoin rate</p>
+            <Prices bpi={props.bpi}/>
             </div>
         </Layout>
     );
+};
+
+index.getInitialProps = async function () {
+    const res = await Fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+    return {
+        bpi: data.bpi
+    }
 };
 
 export default index;
