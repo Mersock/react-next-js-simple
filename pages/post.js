4dcc9b463c46e22fee60c1d4552d8b0794edf1c7
Phumthawan <knzphumthawan@gmail.com>
2019-05-29 03:41:55 +0700

    add style post

--- pages/post.js
+++ pages/post.js
@@ -3,7 +3,7 @@ import {withRouter} from 'next/router'
 
 const post = ({url}) => (
     <Layout title={url.query.title}>
-        <p>Enim commodo in ad proident proident tempor incididunt occaecat do est enim mollit. Laborum amet minim dolor ullamco id amet fugiat magna anim ad cupidatat laborum. Eiusmod aliquip Lorem anim in sunt non nisi consectetur ut reprehenderit duis anim.
+        <p style={{ width:'80vw' }}>Enim commodo in ad proident proident tempor incididunt occaecat do est enim mollit. Laborum amet minim dolor ullamco id amet fugiat magna anim ad cupidatat laborum. Eiusmod aliquip Lorem anim in sunt non nisi consectetur ut reprehenderit duis anim.
         Consequat nostrud sint id reprehenderit sunt minim ipsum cillum. Dolore duis sint anim cillum laborum velit dolor nostrud pariatur nisi non mollit tempor ad. Magna tempor sint nisi officia sint do magna minim. Cillum quis non eiusmod sit in. Sit sint et voluptate duis adipisicing ut eu est aliquip incididunt sunt ex exercitation. Cillum reprehenderit esse qui magna sint consectetur laboris velit eiusmod irure nisi.
         </p>
     </Layout>
